52fa77c1ce174ac3e27152fdf8f0482a19cb1ede
maany <imptodefeat@gmail.com>
2015-11-15 05:42:31 +0530

    RGB retrieved and modified - smoke test successful

--- app/src/main/java/com/maany/disabilityemaulator/ColorblindnessTestFilter.java
+++ app/src/main/java/com/maany/disabilityemaulator/ColorblindnessTestFilter.java
@@ -3,6 +3,7 @@ package com.maany.disabilityemaulator;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.graphics.ImageFormat;
 import android.graphics.Rect;
 import android.graphics.YuvImage;
@@ -19,12 +20,12 @@ import java.io.ByteArrayOutputStream;
  * 1st param is byte[] i.e raw data from camera.
  * Created by Mayank on 11/14/2015.
  */
-public class ColorblindnessTestFilter extends AsyncTask<byte[],Integer,byte[]>{
+public class ColorblindnessTestFilter extends AsyncTask<byte[], Integer, byte[]> {
     private ImageView imageView;
     private Camera camera;
     private Context context;
 
-    public ColorblindnessTestFilter(Context context,ImageView imageView, Camera camera) {
+    public ColorblindnessTestFilter(Context context, ImageView imageView, Camera camera) {
         this.context = context;
         this.imageView = imageView;
         this.camera = camera;
@@ -32,14 +33,10 @@ public class ColorblindnessTestFilter extends AsyncTask<byte[],Integer,byte[]>{
 
     @Override
     protected byte[] doInBackground(byte[]... params) {
-
-
         byte[] data = params[0];
-   //     return data;
-
-    Camera.Size previewSize = camera.getParameters().getPreviewSize();
+        Camera.Size previewSize = camera.getParameters().getPreviewSize();
  /*       Bitmap bm = BitmapFactory.decodeByteArray(data, 0, data.length);
-       ByteArrayOutputStream stream = new ByteArrayOutputStream();
+          ByteArrayOutputStream stream = new ByteArrayOutputStream();
         bm.compress(Bitmap.CompressFormat.PNG, 100, stream);
         byte[] byteArray = stream.toByteArray();
         return byteArray;*/
@@ -67,8 +64,14 @@ public class ColorblindnessTestFilter extends AsyncTask<byte[],Integer,byte[]>{
             }}
 */
 
+        //Bitmap
 
-        YuvImage yuvimage=new YuvImage(data, ImageFormat.NV21, previewSize.width, previewSize.height, null);
+        //RGB mods
+
+        //Reconstruct to byte array
+
+        //apply yuv image to that bytearray
+        YuvImage yuvimage = new YuvImage(data, ImageFormat.NV21, previewSize.width, previewSize.height, null);
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         yuvimage.compressToJpeg(new Rect(0, 0, 480, 340), 80, baos);
 
@@ -80,17 +83,33 @@ public class ColorblindnessTestFilter extends AsyncTask<byte[],Integer,byte[]>{
 
     @Override
     protected void onPostExecute(byte[] data) {
-        Bitmap bm = BitmapFactory.decodeByteArray(data, 0, data.length);
-        DisplayMetrics dm = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(dm);
+        Bitmap bmSrc = BitmapFactory.decodeByteArray(data, 0, data.length);
+        Bitmap bm = bmSrc.copy(Bitmap.Config.ARGB_8888,true);
+        // Process pixels
+        int pixel,R,G,B,A;
+        for(int i=0;i<bm.getWidth();i++)
+            for(int j=0;j<bm.getHeight();j++){
+                pixel = bm.getPixel(i, j);
+                R = Color.red(pixel);
+                G = Color.green(pixel);
+                B = Color.blue(pixel);
 
-      //  imageView.setMinimumHeight(dm.heightPixels);
-      //  imageView.setMinimumWidth(dm.widthPixels);
+                try {
+                    bm.setPixel(i, j, R);
+                } catch(Exception ex){
+                    ex.printStackTrace();
+                }
+            }
+//        DisplayMetrics dm = new DisplayMetrics();
+//        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        //  imageView.setMinimumHeight(dm.heightPixels);
+        //  imageView.setMinimumWidth(dm.widthPixels);
         imageView.setImageBitmap(bm);
 
     }
 
-    private WindowManager getWindowManager(){
-        return (WindowManager)context.getSystemService(Context.WINDOW_SERVICE);
+    private WindowManager getWindowManager() {
+        return (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
     }
 }
